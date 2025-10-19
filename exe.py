c6f8135233da2e26ab11f19b8b3b6629b23e57cc
Ramona Enzi <ramona.enzi@edu.fh-joanneum.at>
2025-10-19 13:50:10 +0200

    add add_exercise function and minor changes to add_student

--- exe.py
+++ exe.py
@@ -13,14 +13,44 @@ def add_student(students, exercises):
 
     grades = {}
     for ex in exercises:
-        points = float(input("enter the points for the exercise: "))
+        while True:
+            try:
+                points = float(input(f"enter the points for exercise {ex}: "))
+                break
+            except ValueError:
+                print("please enter a number")
         grades[ex] = points
 
     students[number] = {"first_name": first, "last_name": last, "grades": grades}
 
     print("student added")
 
+def add_exercise(students, exercises):
+    ex_name = input("enter the name of the exercise: ")
 
+    if not ex_name:
+        print("exercise name cannot be empty")
+        return
+
+    if ex_name in exercises:
+        print("exercise already exists")
+        return
+
+    exercises.append(ex_name)
+
+    for number, info in students.items():
+        while True:
+            try:
+                points = float(input(f"enter points for {info['first_name']} {info['last_name']}: "))
+                if points < 0:
+                    print("points cannot be negative")
+                    continue
+                break
+            except ValueError:
+                print("please enter a number")
+        info["grades"][ex_name] = points
+
+    print("exercise and grades added")
 
 
 
@@ -29,6 +59,7 @@ while True:
     print("2) add exercise grading")
     print("3) view exercise")
     print("4) view student")
+    print("5) exit")
 
     choice = input("choose an option: ")
 
@@ -39,3 +70,6 @@ while True:
         # if statement for the rest of the options will be written when the rest of the functions view student and exercise are ready
         # 3 view exercise
         # 4 view student
+    elif choice == "5":
+        print("Goodbye!")
+        break
